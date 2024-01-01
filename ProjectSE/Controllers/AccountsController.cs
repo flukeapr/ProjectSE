264fa521a8f17c65f7e0d11e735797c865f77bd0
flukeapr <wngsphlc@gmail.com>
2024-01-01 23:07:01 +0700

    Add project files.

---
+++ ProjectSE/Controllers/AccountsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjectSE.Models;
+
+
+namespace ProjectSE.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private DatabaseSEEntities db = new DatabaseSEEntities();
+
+        // GET: Accounts
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Account model)
+        {
+            
+            
+              var user = db.Accounts.FirstOrDefault(x => x.userName == model.userName && x.password == model.password );
+                if (user != null)
+                {
+                    if (user.role == 1)
+                    {
+                    return RedirectToAction("Index", "Home");
+                }
+                    else if (user.role == 3)
+                    {
+                        return RedirectToAction("About", "Home");
+                    }
+                else
+                {
+                    
+                    return View();
+                }
+                }
+                
+                return View();
+            }  
+           
+        }
+    }
+
---
+++ ProjectSE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectSE.MvcApplication" Language="C#" %>
